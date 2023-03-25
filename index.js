4932dd451e15870f5d1d3f3085e905f22e8e7196
JoaoLuizMelo <joaoluiz.civil@gmail.com>
2023-03-25 20:23:42 -0300

    Adição do Middleware

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const port = 3000
 app.listen(port)
 
 
+
 const orders = []
+
+const checkId = (request,response,next)=>{
+    const { id } = request.params
+    const index = orders.findIndex(order => order.id === id)
+    if (index < 0 ) {
+        return response.status(404).json({ message:"This order are not found."})
+    }
+    request.orderIndex=index
+    request.orderId=id
+    next()
+   
+}
+
 app.get('/order',(request,require)=>{
     return require.json(orders)
 })
@@ -18,32 +32,24 @@ app.post('/order',(request,response)=>{
     return response.status(201).json(order)
 })
 
-app.put('/order/:id',(request,response)=>{
-    const { id } = request.params
+
+app.put('/order/:id',checkId,(request,response)=>{
+    const index = request.orderIndex
     const {name, ordered, value}=request.body
-    const index = orders.findIndex(order => order.id === id)
-    const newOrder = {id, name, ordered, value, status:"Em preparação" }
-    if (index < 0 ) {
-        return response.status(404).json({ message:"This order are not found."})
-    }
+    const newOrder = {id:request.orderId, name, ordered, value, status:"Em preparação" }
     orders[index]=newOrder
     return response.json(newOrder)
 })
 
-app.patch('/order/:id',(request,response)=>{
-    const { id } = request.params
-    const index = orders.findIndex(order => order.id === id)
-    const newOrder = {id, name: orders[index].name, ordered:orders[index].ordered, value:orders[index].value , status:"pronto" }
-    if (index < 0 ) {
-        return response.status(404).json({ message:"This order are not found."})
-    }
+app.patch('/order/:id',checkId,(request,response)=>{
+    const index = request.orderIndex
+    const newOrder = {id:request.orderId, name: orders[index].name, ordered:orders[index].ordered, value:orders[index].value , status:"pronto" }
     orders[index]=newOrder
     return response.status(200).json(newOrder)
 })
 
-app.delete('/order/:id',(request,response)=>{
-    const { id } = request.params
-    const index = orders.findIndex(order => order.id === id)
+app.delete('/order/:id',checkId,(request,response)=>{
+    const index = request.orderIndex
     orders.splice(index,1)
     return response.status(200).json()
 })
